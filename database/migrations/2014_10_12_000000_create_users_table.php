6066bc41e7b01eaa8c770bb45705417a727223df
sslthn31 <alhambra.sulthan@gmail.com>
2024-05-16 15:43:17 +0700

    recover migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -33,7 +33,17 @@ return new class extends Migration
       $table->timestamps();
       $table->softDeletes();
     });
+
+    Schema::create('sessions', function (Blueprint $table) {
+      $table->string('id')->primary();
+      $table->foreignId('user_id')->nullable()->index();
+      $table->string('ip_address', 45)->nullable();
+      $table->text('user_agent')->nullable();
+      $table->longText('payload');
+      $table->integer('last_activity')->index();
+  });
   }
+
 
   /**
    * Reverse the migrations.
