0c879f10cd6ea3338931a383beadd8ab19107fdb
DandanIsmail42 <105099782+DandanIsmail42@users.noreply.github.com>
2024-05-17 09:33:47 +0700

    wip(migration)
    akses ruangan, aktivitas gelang, aktivitas pengunjung, gateway, gateway log, gelang log

---
+++ database/seeders/Gateway.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class Gateway extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
