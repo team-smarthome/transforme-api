84f0ad67e61cf162a76829a1993aa0a3dbddd0b6
sslthn31 <alhambra.sulthan@gmail.com>
2024-05-20 14:24:58 +0700

    create migration

---
+++ database/migrations/2024_05_17_040744_create_gedung_otmils_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('gedung_otmil', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('nama_otmil_id', length: 100);   
+            $table->string('lokasi_otmil_id', length: 100);
+            $table->double('panjang');
+            $table->double('lebar');
+            $table->double('posisi_X');
+            $table->double('posisi_Y');
+            $table->softDeletes();
+
+            // $table->timestamp();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('gedung_otmil');
+    }
+};
