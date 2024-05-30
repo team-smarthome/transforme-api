e541284c550e20c061aa4ac253c20536bf3c8690
DandanIsmail42 <105099782+DandanIsmail42@users.noreply.github.com>
2024-05-30 18:54:50 +0700

    update_ruangan_lemasmil

--- app/Models/RuanganLemasmil.php
+++ app/Models/RuanganLemasmil.php
@@ -11,42 +11,46 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class RuanganLemasmil extends Model
 {
-    use HasFactory, SoftDeletes, HasUuids;
+  use HasFactory, SoftDeletes, HasUuids;
 
-    protected $table = 'ruangan_lemasmil';
-    protected $keyType = 'uuid';
-    public $incrementing = false;
-    public $timestamps = true;
+  protected $table = 'ruangan_lemasmil';
+  protected $keyType = 'uuid';
+  public $incrementing = false;
+  public $timestamps = true;
 
-    protected $fillable = [
-        'nama_ruangan_lemasmil',
-        'jenis_ruangan_lemasmil',
-        'lokasi_lemasmil_id',
-        'zona_id',
-        'panjang',
-        'lebar',
-        'posisi_X',
-        'posisi_Y',
-        'lantai_lemasmil_id'
-    ];
+  protected $fillable = [
+    'nama_ruangan_lemasmil',
+    'jenis_ruangan_lemasmil',
+    'lokasi_lemasmil_id',
+    'zona_id',
+    'panjang',
+    'lebar',
+    'posisi_X',
+    'posisi_Y',
+    'lantai_lemasmil_id'
+  ];
 
-    public function gelang(): HasMany
-    {
-        return $this->hasMany(Gelang::class, 'ruangan_lemasmil_id', 'id');
-    }
+  public function gelang(): HasMany
+  {
+    return $this->hasMany(Gelang::class, 'ruangan_lemasmil_id', 'id');
+  }
+  public function kamera(): HasMany
+  {
+    return $this->hasMany(Kamera::class, 'ruangan_lemasmil_id', 'id');
+  }
 
-    public function lokasiLemasmil(): BelongsTo
-    {
-        return $this->belongsTo(LokasiLemasmil::class, 'lokasi_lemasmil_id', 'id');
-    }
+  public function lokasiLemasmil(): BelongsTo
+  {
+    return $this->belongsTo(LokasiLemasmil::class, 'lokasi_lemasmil_id', 'id');
+  }
 
-    public function zona(): BelongsTo
-    {
-        return $this->belongsTo(Zona::class, 'zona_id', 'id');
-    }
+  public function zona(): BelongsTo
+  {
+    return $this->belongsTo(Zona::class, 'zona_id', 'id');
+  }
 
-    public function lantaiLemasmil(): BelongsTo
-    {
-        return $this->belongsTo(LantaiLemasmil::class, 'lantai_lemasmil_id', 'id');
-    }
+  public function lantaiLemasmil(): BelongsTo
+  {
+    return $this->belongsTo(LantaiLemasmil::class, 'lantai_lemasmil_id', 'id');
+  }
 }
