57e4e8e8036fe0fb54e9ed1df05520daba97d112
Ario Prima <arioprima2@gmail.com>
2024-05-30 16:27:11 +0700

    feat: create data seeder

--- database/seeders/DokumenBapSeeder.php
+++ database/seeders/DokumenBapSeeder.php
@@ -16,10 +16,10 @@ class DokumenBapSeeder extends Seeder
         $dokumenBap = [
             [
                 'id' => '9c2417c1-9bd2-496d-4102-8aec799aee90',
-                'penyidikan_id' => '9c2909a4-82a3-4aee-ac85-f7316584212e',
+                'penyidikan_id' => 'd38bf884-b8b7-4ae4-a8da-ae7dab96d833',
                 'nama_dokumen_bap' => 'bap',
                 'link_dokumen_bap' => 'test',
-                'wbp_profile_id' => '10cfa0a1-5ab3-4daa-9292-29397946316e',
+                'wbp_profile_id' => '12cfa0a1-5ab3-4daa-9292-29397946312w',
                 'saksi_id' => 'e0408528-fab5-443b-91f5-adcf2a420dcc',
                 'created_at' => now(),
                 'updated_at' => now(),
--- database/seeders/VersionSeeder.php
+++ database/seeders/VersionSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str; 
 
 class VersionSeeder extends Seeder
 {
@@ -12,6 +14,15 @@ class VersionSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $version = [
+            [
+                'id' => 'b1b3b3b3-1f81-4329-982c-0c290077846d',
+                'link' => 'ht',
+                'version_name' => "1.0.0",
+                'created_at' => now(),
+            ]
+        ];
+
+        DB::table('version')->insert($version);
     }
 }
