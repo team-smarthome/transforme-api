03fa65f1d651982c369253c95416c3d9dab0052c
DandanIsmail42 <105099782+DandanIsmail42@users.noreply.github.com>
2024-06-06 18:37:08 +0700

    response adn filtering kota, provinsi

--- app/Models/Kota.php
+++ app/Models/Kota.php
@@ -11,31 +11,30 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Kota extends Model
 {
-    use SoftDeletes, HasUuids;
-    protected $fillable = ['nama_kota'];
-    protected $table = 'kota';
-    protected $keyType = 'uuid';
-    public $incrementing = false;
-    public $timestamps = true;
+  use SoftDeletes, HasUuids;
+  protected $fillable = ['nama_kota', 'provinsi_id'];
+  protected $table = 'kota';
+  protected $keyType = 'uuid';
+  public $incrementing = false;
+  public $timestamps = true;
 
-    public function pengadilanMiliter()
-    {
-        return $this->hasMany(PengadilanMiliter::class, 'kota_id', 'id');
-    }
+  public function pengadilanMiliter()
+  {
+    return $this->hasMany(PengadilanMiliter::class, 'kota_id', 'id');
+  }
 
-    public function provinsi()
-    {
-        return $this->belongsTo(Provinsi::class, 'provinsi_id', 'id');
-    }
+  public function provinsi()
+  {
+    return $this->belongsTo(Provinsi::class, 'provinsi_id', 'id');
+  }
 
-    public function kotaWbp(): HasMany
-    {
-        return $this->hasMany(WbpProfile::class, 'kota_id', 'id');
-    }
+  public function kotaWbp(): HasMany
+  {
+    return $this->hasMany(WbpProfile::class, 'kota_id', 'id');
+  }
 
-    public function pengunjung()
-    {
-        return $this->hasMany(Pengunjung::class, 'kota_id', 'id');
-    }
-    
+  public function pengunjung()
+  {
+    return $this->hasMany(Pengunjung::class, 'kota_id', 'id');
+  }
 }
--- routes/api/kota.php
+++ routes/api/kota.php
@@ -1,9 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProvinsiController;
+use App\Http\Controllers\KotaController;
 use App\Http\Middleware\AuthSanctumMiddleware;
 
 Route::middleware([AuthSanctumMiddleware::class . ':operator,admin,superadmin'])->group(function () {
-  Route::get('provinsi', [ProvinsiController::class, 'index']);
+  Route::get('kota', [KotaController::class, 'index']);
 });
