6c05aaaf77a810f65f6e3641d38d9da2fbca9ae5
DandanIsmail42 <105099782+DandanIsmail42@users.noreply.github.com>
2024-06-11 17:10:03 +0700

    update route kamera_tersimpan

--- routes/api/kamera_tersimpan.php
+++ routes/api/kamera_tersimpan.php
@@ -1,17 +1,16 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\KameraTersimpanController;
 use App\Http\Middleware\AuthSanctumMiddleware;
-use Illuminate\Support\Facades\Route;
 
+Route::middleware([AuthSanctumMiddleware::class . ':operator,admin,superadmin'])->group(function () {
+  Route::get('kamera_tersimpan', [KameraTersimpanController::class, 'index']);
+  // Route::get('kasus/{id}', [KameraTersimpanController::class, 'show']);
+});
 
-// Route::middleware([AuthSanctumMiddleware::class . ':admin,superadmin'])->group(function () {
-    Route::get('kamera_tersimpan', [KameraTersimpanController::class, 'index']);
-    Route::post('kamera_tersimpan', [KameraTersimpanController::class, 'store']);
-    Route::put('kamera_tersimpan', [KameraTersimpanController::class, 'update']);
-    Route::delete('kamera_tersimpan', [KameraTersimpanController::class, 'destroy']);
-// });
-
-// Route::middleware([AuthSanctumMiddleware::class . ':operator'])->group(function () {
-//     Route::get('gedung_otmil', [GedungOtmilController::class, 'index']);
-// });
+Route::middleware([AuthSanctumMiddleware::class . ':admin,superadmin'])->group(function () {
+  Route::post('kamera_tersimpan', [KameraTersimpanController::class, 'store']);
+  Route::put('kamera_tersimpan', [KameraTersimpanController::class, 'update']);
+  Route::delete('kamera_tersimpan', [KameraTersimpanController::class, 'destroy']);
+});
