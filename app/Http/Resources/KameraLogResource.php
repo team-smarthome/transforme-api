aeb5b3ff56bc813da2ccf81cd7540c45b5dad492
DandanIsmail42 <105099782+DandanIsmail42@users.noreply.github.com>
2024-06-11 17:09:50 +0700

    update response kamera log

--- app/Http/Resources/KameraLogResource.php
+++ app/Http/Resources/KameraLogResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -12,15 +13,29 @@ class KameraLogResource extends JsonResource
    *
    * @return array<string, mixed>
    */
+
   public function toArray(Request $request): array
   {
+    $tipeLokasi = null;
+    $namaLokasi = null;
+    if ($this->kamera->ruanganOtmil) {
+      $tipeLokasi = 'otmil';
+      $namaLokasi = $this->kamera->ruanganOtmil->lokasiOtmil->nama_lokasi_otmil;
+    } else if ($this->kamera->ruanganLemasmil) {
+      $tipeLokasi = 'lemasmil';
+      $namaLokasi = $this->kamera->ruanganLemasmil->lokasiLemasMil->nama_lokasi_lemasmil;
+    }
     return [
       'kamera_log_id' => $this->id,
       'image' => $this->image,
+      'timestamp' => $this->created_at->format('Y-m-d H:i:s'),
+      'datenow' => Carbon::now('Asia/Jakarta')->format('Y-m-d H:i:s'),
       'kamera_id' => $this->kamera_id,
       'nama_kamera' => $this->kamera->nama_kamera ?? null,
-      'foto_wajah_fr' => $this->foto_wajah_fr,
-      'created_at' => $this->created_at
+      'lokasi_id' => $this->kamera->ruanganOtmil->lokasi_otmil_id ?? $this->kamera->ruanganLemasmil->lokasi_lemasmil_id ?? null,
+      'tipe_lokasi' => $tipeLokasi,
+      'nama_lokasi' => $namaLokasi,
+      'keterangan' => $this->foto_wajah_fr ? 'Dikenal' : 'Tidak Dikenal',
     ];
   }
 }
