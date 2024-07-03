d8ce0a6deef9c515d8bf7b187bfecb4e95b4fa83
Simon071198 <81679661+Simon071198@users.noreply.github.com>
2024-07-03 18:45:28 +0700

    fix api dashboard gedung

--- routes/api/indoor_map.php
+++ routes/api/indoor_map.php
@@ -1,14 +1,14 @@
 <?php
-use App\Http\Controllers\DashboardSummaryController;
+use App\Http\Controllers\IndoorMapController;
 use App\Http\Middleware\AuthSanctumMiddleware;
 use Illuminate\Support\Facades\Route;
 
 
 // Route::middleware([AuthSanctumMiddleware::class . ':admin,superadmin'])->group(function () {
-    Route::get('dashboard', [DashboardSummaryController::class, 'index']);
-    Route::post('dashboard', [DashboardSummaryController::class, 'store']);
-    Route::put('dashboard', [DashboardSummaryController::class, 'update']);
-    Route::delete('dashboard', [DashboardSummaryController::class, 'destroy']);
+    Route::get('indoor_map', [IndoorMapController::class, 'index']);
+    Route::post('indoor_map', [IndoorMapController::class, 'store']);
+    Route::put('indoor_map', [IndoorMapController::class, 'update']);
+    Route::delete('indoor_map', [IndoorMapController::class, 'destroy']);
 // });
 
 // Route::middleware([AuthSanctumMiddleware::class . ':operator'])->group(function () {
