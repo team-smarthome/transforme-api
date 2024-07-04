7f127cfc626dd0646c3617e87f9734812097e72f
RizkiIrwansyah99 <bangiki650@gmail.com>
2024-07-04 17:53:51 +0700

    Update API Master

--- app/Http/Controllers/LantaiController.php
+++ app/Http/Controllers/LantaiController.php
@@ -6,48 +6,59 @@ use Illuminate\Http\Request;
 use App\Helpers\ApiResponse;
 use App\Http\Resources\LantaiOtmilResource;
 use App\Models\LantaiOtmil;
+
 class LantaiController extends Controller
 {
     //
     public function index(Request $request)
     {
         try {
-            $keyword = $request->input('search');
+            // $keyword = $request->input('search');
+            // $pageSize = $request->input('pageSize', ApiResponse::$defaultPagination);
+
+            // $getData = LantaiOtmil::with(['lokasiOtmil', 'gedungOtmil'])->where('nama_lantai', 'LIKE', '%' . $keyword . '%')
+            // ->whereHas('gedungOtmil', function ($q) use ($keyword){
+            //     $q ->where('nama_gedung_otmil', 'LIKE', '%' . $keyword . '%');
+            // })->whereHas('lokasiOtmil', function ($r) use ($keyword){
+            //     $r ->where('nama_lokasi_otmil', 'LIKE', '%' . $keyword . '%');
+            // })->latest()->paginate($pageSize);
+
+            // $resourceCollection = LantaiOtmilResource::collection($getData);
+            $nama_lantai = $request->input('nama_lantai');
+            $nama_gedung = $request->input('nama_gedung_otmil');
             $pageSize = $request->input('pageSize', ApiResponse::$defaultPagination);
 
-            $getData = LantaiOtmil::with(['lokasiOtmil', 'gedungOtmil'])->where('nama_lantai', 'LIKE', '%' . $keyword . '%')
-            ->whereHas('gedungOtmil', function ($q) use ($keyword){
-                $q ->where('nama_gedung_otmil', 'LIKE', '%' . $keyword . '%');
-            })->whereHas('lokasiOtmil', function ($r) use ($keyword){
-                $r ->where('nama_lokasi_otmil', 'LIKE', '%' . $keyword . '%');
-            })->latest()->paginate($pageSize);
+            $query = LantaiOtmil::with(['lokasiOtmil', 'gedungOtmil', 'ruanganOtmil'])
+            ->when($nama_lantai, function ($q) use ($nama_lantai) {
+                $q->where('nama_lantai', 'LIKE', '%' . $nama_lantai . '%');
+            })
+            ->latest()
+            ->paginate($pageSize);
 
-            $resourceCollection = LantaiOtmilResource::collection($getData);
-
+            $resourceCollection = LantaiOtmilResource::collection($query);
             return ApiResponse::pagination($resourceCollection);
         } catch (\Exception $e) {
             return ApiResponse::error('An error occurred while fetching data.', $e->getMessage());
         }
-
     }
 
     public function store(Request $request)
     {
         try {
-         $request->validate([
-            'nama_lantai' => 'required|string',
-            'gedung_otmil_id' => 'required|string',
-            'lokasi_otmil_id' => 'required|string',
-            'panjang' => 'nullable',
-            'lebar' => 'nullable',
-            'posisi_X' => 'nullable',
-            'posisi_Y' => 'nullable'
-         ]);
+            $request->validate([
+                'nama_lantai' => 'required|string',
+                'gedung_otmil_id' => 'required|string',
+                'lokasi_otmil_id' => 'required|string',
+                'panjang' => 'nullable',
+                'lebar' => 'nullable',
+                'posisi_X' => 'nullable',
+                'posisi_Y' => 'nullable'
+            ]);
 
-         $insert = LantaiOtmil::create($request->all());
-         return ApiResponse::created([
-            'data' => $insert
-        ]);
+            $insert = LantaiOtmil::create($request->all());
+            return ApiResponse::created([
+                'data' => $insert
+            ]);
         } catch (\Exception $e) {
             return ApiResponse::error('An error occurred while fetching data.', $e->getMessage());
         }
@@ -73,19 +84,19 @@ class LantaiController extends Controller
             'lebar' => 'nullable',
             'posisi_X' => 'nullable',
             'posisi_Y' => 'nullable'
-         ]);
-         $lantai_otmil_id = $request->input('lantai_otmil_id');
-         $findLantai = LantaiOtmil::where('id', $lantai_otmil_id)->firstOrFail();
-         $findLantai->nama_lantai = $request->input('nama_lantai');
-         $findLantai->lokasi_otmil_id = $request->input('lokasi_otmil_id');
-         $findLantai->gedung_otmil_id = $request->input('gedung_otmil_id');
-         $findLantai->panjang = $request->input('panjang');
-         $findLantai->lebar = $request->input('lebar');
-         $findLantai->posisi_X = $request->input('posisi_X');
-         $findLantai->posisi_Y = $request->input('posisi_Y');
- 
-         $findLantai->save();
-         return ApiResponse::updated();
+        ]);
+        $lantai_otmil_id = $request->input('lantai_otmil_id');
+        $findLantai = LantaiOtmil::where('id', $lantai_otmil_id)->firstOrFail();
+        $findLantai->nama_lantai = $request->input('nama_lantai');
+        $findLantai->lokasi_otmil_id = $request->input('lokasi_otmil_id');
+        $findLantai->gedung_otmil_id = $request->input('gedung_otmil_id');
+        $findLantai->panjang = $request->input('panjang');
+        $findLantai->lebar = $request->input('lebar');
+        $findLantai->posisi_X = $request->input('posisi_X');
+        $findLantai->posisi_Y = $request->input('posisi_Y');
+
+        $findLantai->save();
+        return ApiResponse::updated();
     }
 
     public function destroy(Request $request)
@@ -93,7 +104,7 @@ class LantaiController extends Controller
         $lantai_otmil_id = $request->input('lantai_otmil_id');
         $lantai_otmil = LantaiOtmil::findOrFail($lantai_otmil_id);
         $lantai_otmil->delete();
-    
-        return ApiResponse::deleted();   
+
+        return ApiResponse::deleted();
     }
 }
