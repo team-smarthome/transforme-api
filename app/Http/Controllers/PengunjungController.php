b17b427b78a445d27ee8c439eec6bd467b42b659
Simon071198 <81679661+Simon071198@users.noreply.github.com>
2024-07-11 18:22:50 +0700

    fix image pengunjung

--- app/Http/Controllers/PengunjungController.php
+++ app/Http/Controllers/PengunjungController.php
@@ -8,6 +8,9 @@ use App\Http\Requests\PengunjungRequest;
 use App\Helpers\ApiResponse;
 use App\Http\Resources\PengunjungResource;
 use Exception;
+use Ramsey\Uuid\Uuid;
+use App\Helpers\Helpers;
+use Illuminate\Support\Facades\DB;
 
 class PengunjungController extends Controller
 {
@@ -62,8 +65,13 @@ class PengunjungController extends Controller
      */
     public function store(PengunjungRequest $request)
     {
+        $uuid = Uuid::uuid4()->toString();
+        $base64Image = $request['foto_wajah'];
+        $image = Helpers::HandleImageToBase64($base64Image, 'pengunjung_image');
         try {
-            $pengunjung =  new Pengunjung([
+            DB::beginTransaction();
+            $pengunjung =   Pengunjung::create([
+                'id' => $uuid,
                 'nama' => $request->nama,
                 'tempat_lahir' => $request->tempat_lahir,
                 'tanggal_lahir' => $request->tanggal_lahir,
@@ -71,21 +79,26 @@ class PengunjungController extends Controller
                 'provinsi_id' => $request->provinsi_id,
                 'kota_id' => $request->kota_id,
                 'alamat' => $request->alamat,
-                'foto_wajah' => $request->foto_wajah,
+                'foto_wajah' => $image,
                 'wbp_profile_id' => $request->wbp_profile_id,
                 'hubungan_wbp' => $request->hubungan_wbp,
                 'nik' => $request->nik,
-                'foto_wajah_fr' => $request->foto_wajah_fr
+                'foto_wajah_fr' => $base64Image,
             ]);
+            DB::commit();
+            return response()->json([
+                'status' => 'OK',
+                'message' => 'Successfully created data.',
+            ], 200);
 
-            if ($request->hasFile('foto_wajah')) {
-                $gambarPath = $request->file('foto_wajah')->store('public/pengunjung_image');
-                $pengunjung->foto_wajah = str_replace('public/', '', $gambarPath);
-            }
+            // if ($request->hasFile('foto_wajah')) {
+            //     $gambarPath = $request->file('foto_wajah')->store('public/pengunjung_image');
+            //     $pengunjung->foto_wajah = str_replace('public/', '', $gambarPath);
+            // }
 
-            $pengunjung->save();
+            // $pengunjung->save();
 
-            return ApiResponse::created($pengunjung);
+            // return ApiResponse::created($pengunjung);
         } catch (Exception $e) {
             return ApiResponse::error('Failed to save Data.', $e->getMessage());
         }
@@ -112,6 +125,13 @@ class PengunjungController extends Controller
     public function update(PengunjungRequest $request)
     {
         try {
+            // DB::beginTransaction();
+            // $findPengunjung = Pengunjung::where('id', $request['pengunjung_id'])->first();
+            // $image = $request['foto_wajah'];
+            // if (strpos($image, 'data:image/') === 0 && $image != $findPengunjung->foto_wajah) {
+            //     $image = Helpers::HandleImageToBase64($image, 'pengunjung-image');
+            // }
+            // $data_foto_wajah_fr = $request['foto_wajah'] == $findPengunjung->foto_wajah_fr ? $findPengunjung->foto_wajah_fr : $request['foto_wajah'];
             $id = $request->input('pengunjung_id');
             $pengunjung = Pengunjung::findOrfail($id);
             if (!$pengunjung) {
@@ -129,12 +149,33 @@ class PengunjungController extends Controller
             $pengunjung->nik = $request->nik;
             $pengunjung->foto_wajah_fr = $request->foto_wajah_fr;
 
-            if ($request->hasFile('foto_wajah')) {
-                $gambarPath = $request->file('foto_wajah')->store('public/pengunjung_image');
-                $pengunjung->foto_wajah = str_replace('public/', '', $gambarPath);
-            }
+            // if ($request->hasFile('foto_wajah')) {
+            //     $gambarPath = $request->file('foto_wajah')->store('public/pengunjung_image');
+            //     $pengunjung->foto_wajah = str_replace('public/', '', $gambarPath);
+            // }
 
             $pengunjung->save();
+
+            // $updatePengujung = Pengunjung::where('id', $request['pengunjung_id'])
+            // ->update([
+            // 'nama' => $request['nama'],
+            // 'tempat_lahir' => $request['tempat_lahir'],
+            // 'tanggal_lahir' => $request['tanggal_lahir'],
+            // 'jenis_kelamin' => $request['jenis_kelamin'],
+            // 'provinsi_id' => $request['provinsi_id'],
+            // 'kota_id' => $request['kota_id'],
+            // 'alamat' => $request['alamat'],
+            // 'wbp_profile_id' => $request['wbp_profile_id'],
+            // 'hubungan_wbp' => $request['hubungan_wbp'],
+            // 'nik' => $request['nik'],
+            // 'foto_wajah' => $image,
+            // 'foto_wajah_fr' => $data_foto_wajah_fr,
+            // ]);
+            // DB::commit();
+            // return response()->json([
+            //     'status' => 'OK',
+            //     'message' => 'Successfully updated data.',
+            // ], 201);
 
 
             return ApiResponse::updated($pengunjung);
--- app/Http/Requests/PengunjungRequest.php
+++ app/Http/Requests/PengunjungRequest.php
@@ -35,43 +35,43 @@ class PengunjungRequest extends FormRequest
             'wbp_profile_id'  => 'nullable|string',
             'hubungan_wbp' => 'nullable|string',
             'nik' => 'nullable|string',
-            'foto_wajah_fr' => 'nullable|string',
+            'foto_wajah_fr' => 'nullable',
         ];
     }
 
-    public function failedValidation(Validator $validator)
-    {
-        throw new HttpResponseException(response([
-            "status" => "NO",
-            "message" => "Validation Error",
-            "errors" => $validator->getMessageBag()
-        ], 422));
-    }
+    // public function failedValidation(Validator $validator)
+    // {
+    //     throw new HttpResponseException(response([
+    //         "status" => "NO",
+    //         "message" => "Validation Error",
+    //         "errors" => $validator->getMessageBag()
+    //     ], 422));
+    // }
 
-    public function messages(): array {
-        return [
-            'nama.nullable' => 'Nama harus diisi',
-            'nama.string' => 'Nama harus berupa string',
-            'tempat_lahir.nullable' => 'Tempat Lahir harus diisi',
-            'tempat_lahir.string' => 'Tempat Lahir harus berupa string',
-            'tanggal_lahir.nullable' => 'Tanggal Lahir harus diisi',
-            'tanggal_lahir.date' => 'Tanggal Lahir harus berupa tanggal',
-            'jenis_kelamin.nullable' => 'Jenis Kelamin harus diisi',
-            'jenis_kelamin.string' => 'Jenis Kelamin harus berupa string',
-            'provinsi_id.nullable' => 'Provinsi ID harus diisi',
-            'provinsi_id.uuid' => 'Provinsi ID harus berupa UUID',
-            'provinsi_id.exists' => 'Provinsi ID tidak ditemukan',
-            'kota_id.nullable' => 'Kota ID harus diisi',
-            'kota_id.uuid' => 'Kota ID harus berupa UUID',
-            'kota_id.exists' => 'Kota ID tidak ditemukan',
-            'alamat.string' => 'Alamat harus berupa string',
-            // 'foto_wajah.image' => 'Foto Wajah harus berupa gambar',
-            'wbp_profile_id.nullable' => 'WBP Profile ID harus diisi',
-            'wbp_profile_id.uuid' => 'WBP Profile ID harus berupa UUID',
-            'wbp_profile_id.exists' => 'WBP Profile ID tidak ditemukan',
-            'hubungan_wbp.string' => 'Hubungan WBP harus berupa string',
-            'nik.string' => 'NIK harus berupa string',
-            'foto_wajah_fr.image' => 'Foto Wajah FR harus berupa base64'
-        ];
-    }
+    // public function messages(): array {
+    //     return [
+    //         'nama.nullable' => 'Nama harus diisi',
+    //         'nama.string' => 'Nama harus berupa string',
+    //         'tempat_lahir.nullable' => 'Tempat Lahir harus diisi',
+    //         'tempat_lahir.string' => 'Tempat Lahir harus berupa string',
+    //         'tanggal_lahir.nullable' => 'Tanggal Lahir harus diisi',
+    //         'tanggal_lahir.date' => 'Tanggal Lahir harus berupa tanggal',
+    //         'jenis_kelamin.nullable' => 'Jenis Kelamin harus diisi',
+    //         'jenis_kelamin.string' => 'Jenis Kelamin harus berupa string',
+    //         'provinsi_id.nullable' => 'Provinsi ID harus diisi',
+    //         'provinsi_id.uuid' => 'Provinsi ID harus berupa UUID',
+    //         'provinsi_id.exists' => 'Provinsi ID tidak ditemukan',
+    //         'kota_id.nullable' => 'Kota ID harus diisi',
+    //         'kota_id.uuid' => 'Kota ID harus berupa UUID',
+    //         'kota_id.exists' => 'Kota ID tidak ditemukan',
+    //         'alamat.string' => 'Alamat harus berupa string',
+    //         // 'foto_wajah.image' => 'Foto Wajah harus berupa gambar',
+    //         'wbp_profile_id.nullable' => 'WBP Profile ID harus diisi',
+    //         'wbp_profile_id.uuid' => 'WBP Profile ID harus berupa UUID',
+    //         'wbp_profile_id.exists' => 'WBP Profile ID tidak ditemukan',
+    //         'hubungan_wbp.string' => 'Hubungan WBP harus berupa string',
+    //         'nik.string' => 'NIK harus berupa string',
+    //         'foto_wajah_fr.image' => 'Foto Wajah FR harus berupa base64'
+    //     ];
+    // }
 }
