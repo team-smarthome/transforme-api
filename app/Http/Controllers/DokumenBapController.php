72b41764fe0e13de4c70ea16031df8bafa5689ac
DandanIsmail42 <105099782+DandanIsmail42@users.noreply.github.com>
2024-07-18 18:29:36 +0700

    fix(bap, sidang, wbp): fixing query

--- app/Http/Controllers/DokumenBapController.php
+++ app/Http/Controllers/DokumenBapController.php
@@ -10,127 +10,124 @@ use App\Http\Resources\DokumenBapResource;
 
 class DokumenBapController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index(Request $request)
-    {
-        try {
-            $namaDokumen = $request->input('namaDokumen');
-            $nomorPenyidikan = $request->input('nomorPenyidikan');
-            $namaKasus = $request->input('namaKasus');
-            $query = DokumenBap::with(['penyidikan.kasus', 'wbpProfile.hunianWbpOtmil.lokasiOtmil', 'wbpProfile.hunianWbpLemasmil.lokasiLemasmil', 'saksi'])
-            ->where('nama_dokumen_bap', "LIKE", "%" . $namaDokumen . "%")
-            ->whereHas('penyidikan', function ($q) use ($nomorPenyidikan){
-                $q -> where('nomor_penyidikan', 'LIKE', '%' . $nomorPenyidikan. '%');
-            })
-            ->whereHas('penyidikan.kasus', function ($r) use ($namaKasus){
-                $r -> where('nama_kasus', 'LIKE', '%' . $namaKasus. '%');
-            });
+  /**
+   * Display a listing of the resource.
+   */
+  public function index(Request $request)
+  {
+    try {
+      $namaDokumen = $request->input('namaDokumen');
+      $nomorPenyidikan = $request->input('nomorPenyidikan');
+      $namaKasus = $request->input('namaKasus');
+      $query = DokumenBap::with(['penyidikan.kasus', 'wbpProfile.hunianWbpOtmil.lokasiOtmil', 'wbpProfile.hunianWbpLemasmil.lokasiLemasmil', 'saksi'])
+        ->where('nama_dokumen_bap', "ILIKE", "%" . $namaDokumen . "%")
+        ->whereHas('penyidikan', function ($q) use ($nomorPenyidikan) {
+          $q->where('nomor_penyidikan', 'ILIKE', '%' . $nomorPenyidikan . '%');
+        })
+        ->whereHas('penyidikan.kasus', function ($r) use ($namaKasus) {
+          $r->where('nama_kasus', 'ILIKE', '%' . $namaKasus . '%');
+        });
 
-            $query->latest();
-            $paginatedData = $query->paginate($request->input('pageSize', ApiResponse::$defaultPagination));
+      $query->latest();
+      $paginatedData = $query->paginate($request->input('pageSize', ApiResponse::$defaultPagination));
 
-            $resourceCollection = DokumenBapResource::collection($paginatedData);
+      $resourceCollection = DokumenBapResource::collection($paginatedData);
 
-            return ApiResponse::pagination($resourceCollection, 'Successfully get Data');
-        } catch (\Exception $e) {
-            return ApiResponse::error('Failed to get Data.', $e->getMessage());
-        }
+      return ApiResponse::pagination($resourceCollection, 'Successfully get Data');
+    } catch (\Exception $e) {
+      return ApiResponse::error('Failed to get Data.', $e->getMessage());
     }
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+  /**
+   * Show the form for creating a new resource.
+   */
+  public function create()
+  {
+    //
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(DokumenBapRequest $request)
+  {
+    try {
+      $dokumenBap = new DokumenBap([
+        'penyidikan_id' => $request->penyidikan_id,
+        'nama_dokumen_bap' => $request->nama_dokumen_bap,
+        'wbp_profile_id' => $request->wbp_profile_id,
+        'saksi_id' => $request->saksi_id
+      ]);
+
+      if ($request->hasFile('link_dokumen_bap')) {
+        $dokumenPath = $request->file('link_dokumen_bap')->store('public/link_dokumen_bap_file');
+        $dokumenBap->link_dokumen_bap = str_replace('public/', '', $dokumenPath);
+      }
+
+      $dokumenBap->save();
+      return ApiResponse::created($dokumenBap);
+    } catch (\Exception $e) {
+      return ApiResponse::error('Failed to create Dokumen BAP.', $e->getMessage());
     }
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(DokumenBapRequest $request)
-    {
-        try {
-            $dokumenBap = new DokumenBap([
-                'penyidikan_id' => $request->penyidikan_id,
-                'nama_dokumen_bap' => $request->nama_dokumen_bap,
-                'wbp_profile_id' => $request->wbp_profile_id,
-                'saksi_id' => $request->saksi_id
-            ]);
+  /**
+   * Display the specified resource.
+   */
+  public function show(string $id)
+  {
+    //
+  }
 
-            if ($request->hasFile('link_dokumen_bap')) {
-                $dokumenPath = $request->file('link_dokumen_bap')->store('public/link_dokumen_bap_file');
-                $dokumenBap->link_dokumen_bap = str_replace('public/', '', $dokumenPath);
-            }
+  /**
+   * Show the form for editing the specified resource.
+   */
+  public function edit(string $id)
+  {
+    //
+  }
 
-            $dokumenBap->save();
-            return ApiResponse::created($dokumenBap);
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(Request $request)
+  {
+    try {
+      // return $request;
+      // exit();
+      $id = $request->input('dokumen_bap_id');
+      $dokumenBap = DokumenBap::where('id', $id)->firstOrFail();
+      $dokumenBap->penyidikan_id = $request->penyidikan_id;
+      $dokumenBap->nama_dokumen_bap = $request->nama_dokumen_bap;
+      $dokumenBap->wbp_profile_id = $request->wbp_profile_id;
+      $dokumenBap->saksi_id = $request->saksi_id;
 
-        } catch (\Exception $e) {
-            return ApiResponse::error('Failed to create Dokumen BAP.', $e->getMessage());
-        }
+      if ($request->hasFile('link_dokumen_bap')) {
+        $dokumenPath = $request->file('link_dokumen_bap')->store('public/link_dokumen_bap_file');
+        $dokumenBap->link_dokumen_bap = str_replace('public/', '', $dokumenPath);
+      }
+
+      $dokumenBap->save();
+      return ApiResponse::updated($dokumenBap);
+    } catch (\Exception $e) {
+      return ApiResponse::error('Failed to update Dokumen BAP.', $e->getMessage());
     }
+  }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy(Request $request)
+  {
+    try {
+      $id = $request->input('dokumen_bap_id');
+      $dokumenBap = DokumenBap::find($id);
+      $dokumenBap->delete();
+
+      return ApiResponse::deleted();
+    } catch (\Exception $e) {
+      return ApiResponse::error('Failed to delete Dokumen BAP.', $e->getMessage(), 500);
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request)
-    {
-        try {
-            // return $request;
-            // exit();
-            $id = $request->input('dokumen_bap_id');
-            $dokumenBap = DokumenBap::where('id', $id)->firstOrFail();
-            $dokumenBap->penyidikan_id = $request->penyidikan_id;
-            $dokumenBap->nama_dokumen_bap = $request->nama_dokumen_bap;
-            $dokumenBap->wbp_profile_id = $request->wbp_profile_id;
-            $dokumenBap->saksi_id = $request->saksi_id;
-
-            if ($request->hasFile('link_dokumen_bap')) {
-                $dokumenPath = $request->file('link_dokumen_bap')->store('public/link_dokumen_bap_file');
-                $dokumenBap->link_dokumen_bap = str_replace('public/', '', $dokumenPath);
-            }
-
-            $dokumenBap->save();
-            return ApiResponse::updated($dokumenBap);
-
-        } catch (\Exception $e) {
-            return ApiResponse::error('Failed to update Dokumen BAP.', $e->getMessage());
-        }
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Request $request)
-    {
-        try {
-            $id = $request->input('dokumen_bap_id');
-            $dokumenBap = DokumenBap::find($id);
-            $dokumenBap->delete();
-
-            return ApiResponse::deleted();
-            
-        } catch (\Exception $e) {
-            return ApiResponse::error('Failed to delete Dokumen BAP.', $e-> getMessage(), 500);
-        }
-    }
+  }
 }
