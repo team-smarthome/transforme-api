7bca3b1112bc3c7a870c5e74125833a7281557bf
RizkiIrwansyah99 <bangiki650@gmail.com>
2024-07-25 21:16:24 +0700

    MstManufacture

---
+++ app/Http/Controllers/MstManufacturerController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Helpers\ApiResponse;
+use App\Http\Controllers\Controller;
+use App\Models\Mst_manufacturer;
+use Exception;
+use Illuminate\Http\Request;
+
+class MstManufacturerController extends Controller
+{
+    public function index()
+    {
+
+    }
+}
