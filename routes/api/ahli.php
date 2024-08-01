7575095f087cea4447104b08a979d62eb65fdc8b
arioprima <arioprima2@gmail.com>
2024-08-01 14:02:19 +0700

    test ci cd

--- routes/api/ahli.php
+++ routes/api/ahli.php
@@ -6,7 +6,6 @@ use App\Http\Middleware\AuthSanctumMiddleware;
 
 Route::middleware([AuthSanctumMiddleware::class . ':operator,admin,superadmin'])->group(function () {
     Route::get('ahli', [AhliController::class, 'index']);
-    // Route::get('ahli/{id}', [AhliController::class, 'show']);
 });
 
 Route::middleware([AuthSanctumMiddleware::class . ':admin,superadmin'])->group(function () {
